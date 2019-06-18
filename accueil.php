f931af4607178d345dcd7552d25f7376ccd2a4b1
rbm94 <45996521+rbm94@users.noreply.github.com>
2019-06-18 16:08:18 +0200

    hypno
    includes

--- accueil.php
+++ accueil.php
@@ -1,32 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
-
-<!-- Lien fontawesome -->
-   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-<!-- Lien CDN CSS -->
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<!-- Lien CSS personel -->
-<link rel="stylesheet" href="lib/css/style.css">
-
-</head>
-<body>
-
-<div class="container">
-    <header class="row">
-        <div class="col-md-2">logo</div>
-        <div class="col-md-2">1ere adresse</div>
-        <div class="col-md-2">2eme adresse</div>
-        <div class="col-md-2">Fonction</div>
-        <div class="col-md-2">Prise de RDV</div>
-        <div class="col-md-2">tel</div>
-    </header><!-- Fin header -->
-
-<main class="row">
+<?php
+include_once 'inc/header.inc.php';
+?>
     <div class=" col-md-2" >Anne-Cécile ROUGIER <br> Hypno-thérapeute</div>
 
     <div class="col-md-10">
@@ -45,20 +19,11 @@
     </div>
          
         
-</main> <!-- Fin main -->    
 
 
-
-    <footer class="row">
-        <div class="col-md-12">
-        <a href="#" class="col-md-4 btn">fb</a>
-        <a href="#" class="col-md-4 btn">link</a>
-        <a href="#" class="col-md-3 btn">twitter</a>
-        </div>
-    </footer><!-- Fin footer -->
-
-    
-</div> <!-- Fin div container -->
+<?php
+ include_once 'inc/footer.inc.php';
+ ?>
 
 
 
@@ -78,12 +43,3 @@
 
 
 
-
-
-
-<!-- Lien CDN JS -->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>    
-</body>
-</html>
